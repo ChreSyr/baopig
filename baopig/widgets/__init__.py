09bfe23eae3718c6ab908bb84c3c0b2e5bd23a3d
ChreSyr <symrb26@gmail.com>
2022-06-27 14:52:13 +0200

    Removed TextLabel

--- baopig/widgets/__init__.py
+++ baopig/widgets/__init__.py
@@ -1,13 +1,11 @@
 from .scrollablezone import ScrollableZone, ScrollSlider
-from .text          import *
+from .text          import DynamicText, Text
 from .textedit      import TextEdit
 from .lineedit      import LineEdit
 from .entry         import Entry  # TODO : remove ? is it abstract ?
 from .numentry      import NumEntry
-# from .label         import *
 from .button        import Button, ButtonText
 from .checkbox      import CheckBox
-# from .inputbox      import *
 from .progressbar   import ProgressBar
 from .slider        import Slider, SliderBloc, SliderBar
 from .dialog        import Dialog, DialogFrame, DialogButtonsZone, DialogAnswerButton
