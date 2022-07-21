8d601004b13e4d372e34ed7b92dd004378bc83bc
ACER-SyméonRB <dev.chresyr@gmail.com>
2022-07-21 23:20:56 +0200

    Set version to 0.19.0

--- baopig/version/version.py
+++ baopig/version/version.py
@@ -20,7 +20,7 @@ class BaopigVersion(tuple):
     patch = property(lambda self: self[2])
 
 
-version = BaopigVersion(0, 11, 4)
+version = BaopigVersion(0, 19, 0)
 
 if __name__ == "__main__":
     # TODO : try to access baopig.version without creating application, threads...
